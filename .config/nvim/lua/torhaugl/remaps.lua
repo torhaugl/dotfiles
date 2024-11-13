803b90b3aad486d3facdda8bf6113b26512b3419
Tor S. Haugland <tor.haugland@sintef.no>
2024-11-13 09:41:19 +0100

    Update nvim

--- .config/nvim/lua/torhaugl/remaps.lua
+++ .config/nvim/lua/torhaugl/remaps.lua
@@ -15,3 +15,10 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>rr", ":vs <CR> :term python % <CR>")
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {clear=true}),
+    callback = function ()
+        vim.highlight.on_yank()
+    end,
+})
