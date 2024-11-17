71f91d28dfeb2dc46f4a67d0e35aea7d6412b890
Tor S. Haugland <tor.haugland@sintef.no>
2024-11-17 21:18:16 +0100

    Neovim+tmux

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- TmuxNavigator. Add keymaps to go between neovim and tmux
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+    },
+    keys = {
+      { '<C-h>', '<cmd><C-U>TmuxNavigateLeft<CR>' },
+      { '<C-j>', '<cmd><C-U>TmuxNavigateDown<CR>' },
+      { '<C-k>', '<cmd><C-U>TmuxNavigateUp<CR>' },
+      { '<C-l>', '<cmd><C-U>TmuxNavigateRight<CR>' },
+    },
+  },
+}
