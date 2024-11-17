cd3789ba1a3bc7d789fdc5c944d0752ec1ffc2b8
Tor S. Haugland <tor.haugland@sintef.no>
2024-11-17 11:31:19 +0100

    Add .bashrc_local

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -870,5 +870,12 @@ require('lazy').setup({
   },
 })
 
+-- Set syntax for .bashrc_local
+vim.filetype.add {
+  filename = {
+    ['.bashrc_local'] = 'bash',
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
