c1b7d4b72a047a213c2358aa21f4bb090912c7a1
Tor S. Haugland <tor.haugland@sintef.no>
2026-05-04 10:36:06 +0200

    Remove automatic newline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -81,6 +81,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- disable automatic newline comment continuation
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
 -- Show diagnostics when holding cursor for a second.
 -- vim.api.nvim_create_autocmd('CursorHold', {
 --   callback = function()
